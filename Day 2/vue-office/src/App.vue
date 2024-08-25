951effae7948d6c21180d922d449a38e46069ffc
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-08-25 17:03:26 +0500

    chore: day 2

---
+++ Day 2/vue-office/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div v-if="data">
+  <!-- <ul>
+    <li v-for="item in data">
+      {{ item.name }}
+    </li>
+  </ul> -->
+  <Item v-for="item in data" :name="item.name"></Item>
+</div>
+
+</template>
+
+<script setup>
+
+import { ref, onMounted } from 'vue';
+import Item from './Item.vue'
+const data = ref(null)
+onMounted(()=>{
+  fetch('/api/resource/Employee', {
+    method: 'GET',
+    headers:{
+      'Authorization':'Token 014f885e44c7fd9:6cc066db28b9345',
+      'Content-Type':'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(message =>{
+    data.value = message.data
+  })
+  .catch(error=>{
+    console.log('Error', error)
+  })
+})
+
+</script>
---
+++ Day 2/vue-office/src/Item.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>{{ props.name}}</h1>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: String
+})
+</script>
