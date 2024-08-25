12ecdba00575798a81fa9652a0fd32276e67e3b3
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-08-25 17:58:53 +0500

    chore: configure tailwind with vue.js

--- Day 2/vue-office/src/App.vue
+++ Day 2/vue-office/src/App.vue
@@ -1,35 +1,22 @@
 <template>
-<div v-if="data">
-  <!-- <ul>
-    <li v-for="item in data">
-      {{ item.name }}
-    </li>
-  </ul> -->
-  <Item v-for="item in data" :name="item.name"></Item>
-</div>
+
+  <header>
+    <nav>
+      <ul>
+        <li class="text-3xl font-bold underline">Home</li>
+        <li>About</li>
+        
+      </ul>
+    </nav>
+  </header>
+  <List />
+
 
 </template>
 
 <script setup>
 
-import { ref, onMounted } from 'vue';
-import Item from './Item.vue'
-const data = ref(null)
-onMounted(()=>{
-  fetch('/api/resource/Employee', {
-    method: 'GET',
-    headers:{
-      'Authorization':'Token 014f885e44c7fd9:6cc066db28b9345',
-      'Content-Type':'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(message =>{
-    data.value = message.data
-  })
-  .catch(error=>{
-    console.log('Error', error)
-  })
-})
+
+import List from './List.vue'
 
 </script>
---
+++ Day 2/vue-office/src/List.vue
@@ -0,0 +1,25 @@
+<template>
+    <Item v-for="item in data" :name="item.name"></Item>
+</template>
+
+<script setup>
+import Item from './Item.vue'
+import { ref, onMounted } from 'vue';
+const data = ref(null)
+onMounted(()=>{
+  fetch('/api/resource/Employee', {
+    method: 'GET',
+    headers:{
+      'Authorization':'Token 014f885e44c7fd9:6cc066db28b9345',
+      'Content-Type':'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(message =>{
+    data.value = message.data
+  })
+  .catch(error=>{
+    console.log('Error', error)
+  })
+})
+</script>
